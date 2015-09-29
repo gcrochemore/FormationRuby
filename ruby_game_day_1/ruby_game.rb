8825290bd29873fa496ca62ba0592a5ba3941a57
gcrochemore <guillaume.crochemore@chabe-limousines.com>
2015-09-29 12:17:51 +0200

    Test sur les fonctions

--- ruby_game_day_1/ruby_game.rb
+++ ruby_game_day_1/ruby_game.rb
@@ -6,3 +6,26 @@ for j in (1..rows_max)
 	end
 	puts
 end
+
+a=(3+3)
+puts "a vaut " + a.to_s + " est " + a.class.to_s
+
+a=(3+3)*99999*9999999
+puts "a vaut " + a.to_s + " est " + a.class.to_s
+
+test = false
+i = 1073742000
+while test==false
+	if i.class == Bignum
+		puts "Le premier Bignum est " + i.to_s
+		test = true
+	end
+	i += 1
+end
+
+def hello_world(name = "World")
+	puts "Hello #{name}"
+end
+
+hello_world("Guillaume")
+hello_world
