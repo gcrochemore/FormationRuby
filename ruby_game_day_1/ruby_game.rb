a83972be074087c3f42aff1001bca57b03d8a5e6
gcrochemore <guillaume.crochemore@chabe-limousines.com>
2015-09-29 13:57:09 +0200

    Remplacement des clés de dictionnaire par des symboles

--- ruby_game_day_1/ruby_game.rb
+++ ruby_game_day_1/ruby_game.rb
@@ -1,12 +1,21 @@
-def draw(rows_max = 10,cols_max = 10)
-	for j in (1..rows_max)
-		for i in (1..cols_max)
-			print "X "
+def draw(rows_max = 10,cols_max = 10, player = {:x=>1,:y=>1})
+	puts "On affiche un plateau de #{rows_max} lignes et #{cols_max} colonnes"
+	puts "Le joueur est en position #{player[:x]}/#{player[:y]}"
+	for j in (1..rows_max.to_i)
+		for i in (1..cols_max.to_i)
+			if player[:x] == j && player[:y] == i 
+				print "\033[34mP\033[0m "
+			else
+				print "\033[10mX\033[0m "
+			end
 		end
 		puts
 	end
 end
-puts "8*9"
-draw(8,9)
-puts "Par default"
-draw
+
+
+if !ARGV.empty?
+	draw(ARGV[0],ARGV[1],{:x=>ARGV[2].to_i,:y=>ARGV[3].to_i})
+else
+	draw
+end
