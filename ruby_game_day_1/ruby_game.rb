d29b7ae5f730b6cf325164a4772cf6a0d7db9be4
gcrochemore <guillaume.crochemore@chabe-limousines.com>
2015-09-29 12:19:29 +0200

    Création d'une fonction pour dessiner le plateau

--- ruby_game_day_1/ruby_game.rb
+++ ruby_game_day_1/ruby_game.rb
@@ -1,31 +1,12 @@
-cols_max = 10
-rows_max = 10
-for j in (1..rows_max)
-	for i in (1..cols_max)
-		print "X "
+def draw(rows_max = 10,cols_max = 10)
+	for j in (1..rows_max)
+		for i in (1..cols_max)
+			print "X "
+		end
+		puts
 	end
-	puts
 end
-
-a=(3+3)
-puts "a vaut " + a.to_s + " est " + a.class.to_s
-
-a=(3+3)*99999*9999999
-puts "a vaut " + a.to_s + " est " + a.class.to_s
-
-test = false
-i = 1073742000
-while test==false
-	if i.class == Bignum
-		puts "Le premier Bignum est " + i.to_s
-		test = true
-	end
-	i += 1
-end
-
-def hello_world(name = "World")
-	puts "Hello #{name}"
-end
-
-hello_world("Guillaume")
-hello_world
+puts "8*9"
+draw(8,9)
+puts "Par default"
+draw
